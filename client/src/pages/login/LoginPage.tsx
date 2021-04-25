8227da303c12a75bcc58b67da6d5385a52fe3c24
Ric Donati <rdonati5645@gmail.com>
2021-04-25 13:36:24 -0400

    Styling

--- client/src/pages/login/LoginPage.tsx
+++ client/src/pages/login/LoginPage.tsx
@@ -24,17 +24,13 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-  root: {
-    height: '100%',
-    display: 'grid',
-    gridTemplateRows: '1fr auto 1fr',
-  },
   alignVertically: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
   },
   alert: {
+    margin: theme.spacing(3, 0),
     height: '3rem',
   },
 }))
@@ -55,64 +51,62 @@ export default function LoginPage() {
 
   return (
     <Container maxWidth='sm'>
-      <Box component='main' className={classes.root}>
-        <Box className={classes.alignVertically}>
-          <Alert className={classes.alert} style={{ visibility: !!feedback ? 'visible' : 'hidden' }} severity='error'>
-            {feedback}
-          </Alert>
-        </Box>
-        <div className={classes.paper}>
-          <Typography component='h1' variant='h5'>
-            Sign in
-          </Typography>
-
-          <form onSubmit={e => e.preventDefault()} className={classes.form} noValidate>
-            <TextField
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              variant='outlined'
-              margin='normal'
-              required
-              fullWidth
-              id='email'
-              label='Email Address'
-              name='email'
-              autoComplete='email'
-              autoFocus
-            />
-            <TextField
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              variant='outlined'
-              margin='normal'
-              required
-              fullWidth
-              name='password'
-              label='Password'
-              type='password'
-              id='password'
-              autoComplete='current-password'
-            />
-            <Button type='submit' onClick={handleLogin} fullWidth variant='contained' color='primary' className={classes.submit}>
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href='#' variant='body2'>
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link component={RouterLink} to='/register' variant='body2'>
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        {/* PLACEHOLDER */}
-        <div />
+      <Box className={classes.alignVertically}>
+        <Alert className={classes.alert} style={{ visibility: !!feedback ? 'visible' : 'hidden' }} severity='error'>
+          {feedback}
+        </Alert>
       </Box>
+      <div className={classes.paper}>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+
+        <form onSubmit={e => e.preventDefault()} className={classes.form} noValidate>
+          <TextField
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+          />
+          <TextField
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
+          />
+          <Button type='submit' onClick={handleLogin} fullWidth variant='contained' color='primary' className={classes.submit}>
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href='#' variant='body2'>
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link component={RouterLink} to='/register' variant='body2'>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      {/* PLACEHOLDER */}
+      <div />
     </Container>
   )
 }
--- client/src/pages/register/RegisterPage.tsx
+++ client/src/pages/register/RegisterPage.tsx
@@ -20,17 +20,13 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-  root: {
-    height: '100%',
-    display: 'grid',
-    gridTemplateRows: '1fr auto 1fr',
-  },
   alignVertically: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
   },
   alert: {
+    margin: theme.spacing(3, 0),
     height: '3rem',
   },
 }))
@@ -53,87 +49,85 @@ export default function RegisterPage() {
 
   return (
     <Container maxWidth='sm'>
-      <Box component='main' className={classes.root}>
-        <Box className={classes.alignVertically}>
-          <Alert className={classes.alert} style={{ visibility: !!feedback ? 'visible' : 'hidden' }} severity='error'>
-            {feedback}
-          </Alert>
-        </Box>
-        <div className={classes.paper}>
-          <Typography component='h1' variant='h5'>
-            Sign up
-          </Typography>
-          <form onSubmit={e => e.preventDefault()} className={classes.form} noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  value={username}
-                  onChange={e => setUsername(e.target.value)}
-                  autoComplete='uname'
-                  name='username'
-                  variant='outlined'
-                  required
-                  fullWidth
-                  id='username'
-                  label='Username'
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                  variant='outlined'
-                  required
-                  fullWidth
-                  id='email'
-                  label='Email Address'
-                  name='email'
-                  autoComplete='email'
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                  variant='outlined'
-                  required
-                  fullWidth
-                  name='password'
-                  label='Password'
-                  type='password'
-                  id='password'
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  value={password2}
-                  onChange={e => setPassword2(e.target.value)}
-                  variant='outlined'
-                  required
-                  fullWidth
-                  name='password2'
-                  label='Re-enter Password'
-                  type='password'
-                  id='password2'
-                />
-              </Grid>
-            </Grid>
-            <Button type='submit' onClick={handleRegister} fullWidth variant='contained' color='primary' className={classes.submit}>
-              Sign Up
-            </Button>
-            <Grid container justify='flex-end'>
-              <Grid item>
-                <Link component={RouterLink} to='/login' variant='body2'>
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        {/* PLACEHOLDER */}
-        <div />
+      <Box className={classes.alignVertically}>
+        <Alert className={classes.alert} style={{ visibility: !!feedback ? 'visible' : 'hidden' }} severity='error'>
+          {feedback}
+        </Alert>
       </Box>
+      <div className={classes.paper}>
+        <Typography component='h1' variant='h5'>
+          Sign up
+        </Typography>
+        <form onSubmit={e => e.preventDefault()} className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                autoComplete='uname'
+                name='username'
+                variant='outlined'
+                required
+                fullWidth
+                id='username'
+                label='Username'
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                variant='outlined'
+                required
+                fullWidth
+                id='email'
+                label='Email Address'
+                name='email'
+                autoComplete='email'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                variant='outlined'
+                required
+                fullWidth
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                value={password2}
+                onChange={e => setPassword2(e.target.value)}
+                variant='outlined'
+                required
+                fullWidth
+                name='password2'
+                label='Re-enter Password'
+                type='password'
+                id='password2'
+              />
+            </Grid>
+          </Grid>
+          <Button type='submit' onClick={handleRegister} fullWidth variant='contained' color='primary' className={classes.submit}>
+            Sign Up
+          </Button>
+          <Grid container justify='flex-end'>
+            <Grid item>
+              <Link component={RouterLink} to='/login' variant='body2'>
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      {/* PLACEHOLDER */}
+      <div />
     </Container>
   )
 }
